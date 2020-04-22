66a8238b22f4c0b72e4e2ecfb85838461292e160
MyeongchanJin <jmcchan@naver.com>
2020-04-22 22:01:45 +0900

    JSP view 사용 설정, JSP 추가

---
+++ oauth2-with-common-provider/src/main/java/com/npee/controller/HomeController.java
@@ -0,0 +1,24 @@
+package com.npee.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.security.Principal;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("/me")
+    @ResponseBody
+    public Principal getPrincipal(Principal principal) {
+        return principal;
+    }
+}
--- oauth2-with-common-provider/src/main/java/com/npee/controller/OAuth2RestController.java
+++ oauth2-with-common-provider/src/main/java/com/npee/controller/OAuth2RestController.java
@@ -1,17 +1,18 @@
 package com.npee.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/login/auth")
 public class OAuth2RestController {
 
-    @GetMapping("/me")
-    public Principal getPrincipal(Principal principal) {
-        return principal;
+    @GetMapping("/{provider}")
+    public String loginWithProvider(@PathVariable String provider, Principal principal) {
+        return provider;
     }
 }
