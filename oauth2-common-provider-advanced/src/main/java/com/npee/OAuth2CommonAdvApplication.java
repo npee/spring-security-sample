cc0f2ed393de2b9e14036e12febef396c83d50da
MyeongchanJin <jmcchan@naver.com>
2020-04-28 15:20:40 +0900

    실행 클래스 이름 수정

--- oauth2-common-provider-advanced/src/main/java/com/npee/OAuth2CommonAdvApplication.java
+++ oauth2-common-provider-advanced/src/main/java/com/npee/OAuth2CommonAdvApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class OAuth2AdvApplication {
+public class OAuth2CommonAdvApplication {
     public static void main(String[] args) {
-        new SpringApplicationBuilder(OAuth2AdvApplication.class).run(args);
+        new SpringApplicationBuilder(OAuth2CommonAdvApplication.class).run(args);
     }
 }
