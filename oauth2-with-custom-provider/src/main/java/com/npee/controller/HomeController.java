e22765043c90d40eb230770758d139f4020078fe
MyeongchanJin <jmcchan@naver.com>
2020-04-29 20:17:27 +0900

    기본 기능의 컨트롤러

--- oauth2-with-custom-provider/src/main/java/com/npee/controller/HomeController.java
+++ oauth2-with-custom-provider/src/main/java/com/npee/controller/HomeController.java
@@ -1,4 +1,22 @@
 package com.npee.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/")
 public class HomeController {
+
+    @GetMapping
+    public String index() {
+        return "main page";
+    }
+
+    @GetMapping("/me")
+    public Principal getPrincipal(Principal principal) {
+        return principal;
+    }
 }
