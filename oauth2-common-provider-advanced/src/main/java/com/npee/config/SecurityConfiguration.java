459cc8d21d6fef911490dc9953f81e2f3a70b11e
MyeongchanJin <jmcchan@naver.com>
2020-04-30 22:34:20 +0900

    로그인 성공시 리다이렉트 방법 수정

--- oauth2-common-provider-advanced/src/main/java/com/npee/config/SecurityConfiguration.java
+++ oauth2-common-provider-advanced/src/main/java/com/npee/config/SecurityConfiguration.java
@@ -2,12 +2,10 @@ package com.npee.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
@@ -15,13 +13,7 @@ import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserServ
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.security.web.DefaultRedirectStrategy;
-import org.springframework.security.web.RedirectStrategy;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
@@ -30,7 +22,7 @@ import java.util.stream.Collectors;
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    private final RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+    // private final RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
     private final Environment environment;
     private final static String CLIENT_CONFIGURATION_KEY = "spring.security.oauth2.client.registration.";
     private static List<String> providers;
@@ -43,14 +35,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("npee").password("{noop}1234").roles("ADMIN")
-                .and()
-                .withUser("noop").password("{noop}1234").roles("USER");
-    }
-
-    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests(authorizeRequests -> authorizeRequests
@@ -58,12 +42,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .authorizeRequests(authorizeRequests -> authorizeRequests
                         .antMatchers("/me").hasRole("USER"))
                 .authorizeRequests(authorizeRequests -> authorizeRequests
-                        .anyRequest().authenticated())
-                .formLogin().successHandler(authenticationSuccessHandler);
+                        .anyRequest().authenticated());
 
         http
                 .oauth2Login().clientRegistrationRepository(clientRegistrationRepository())
+                .defaultSuccessUrl(targetUrl)
                 .userInfoEndpoint().userService(oAuth2UserService);
+
+        http
+                .logout().logoutUrl("/");
 
     }
 
@@ -76,7 +63,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
         return new InMemoryClientRegistrationRepository(registrations);
     }
-
 
     private ClientRegistration getRegistration(String client) {
 
@@ -105,17 +91,18 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 //        return new InMemoryOAuth2AuthorizedClientService(clientRegistrationRepository());
 //    }
 
-    AuthenticationSuccessHandler authenticationSuccessHandler = new AuthenticationSuccessHandler() {
+//    AuthenticationSuccessHandler authenticationSuccessHandler = new AuthenticationSuccessHandler() {
+//
+//        @Override
+//        public void onAuthenticationSuccess(
+//                HttpServletRequest request, HttpServletResponse response, Authentication authentication
+//        ) throws IOException {
+//            setRedirectUrl(request, response);
+//        }
+//
+//        protected void setRedirectUrl(HttpServletRequest request, HttpServletResponse response) throws IOException {
+//            redirectStrategy.sendRedirect(request, response, targetUrl);
+//        }
+//    };
 
-        @Override
-        public void onAuthenticationSuccess(
-                HttpServletRequest request, HttpServletResponse response, Authentication authentication
-        ) throws IOException {
-            setRedirectUrl(request, response);
-        }
-
-        protected void setRedirectUrl(HttpServletRequest request, HttpServletResponse response) throws IOException {
-            redirectStrategy.sendRedirect(request, response, targetUrl);
-        }
-    };
 }
